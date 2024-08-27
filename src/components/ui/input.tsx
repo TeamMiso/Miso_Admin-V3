5129e059f0009478eed3f904ef9a1109614e1153
송현우 <shw9052@naver.com>
2024-08-27 20:30:53 +0900

    Update input component ui styles

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-0",
+          "font-['Regular'] text-sm flex h-[3.375rem] rounded-[8px] w-full border border-gray-300 bg-background px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus:border-green-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-0",
           className
         )}
         ref={ref}
