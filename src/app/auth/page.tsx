b40ae9de6c500ee312fad9b607d40674562c2d4b
송현우 <shw9052@naver.com>
2024-08-27 20:31:07 +0900

    Update auth page publishing

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -7,22 +7,44 @@ export default function Auth() {
   const { register, handleSubmit } = useForm();
 
   return (
-    <main className="w-full h-full">
-      <form onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}>
-        <Input
-          id="email"
-          type="text"
-          placeholder="이메일"
-          {...register("email")}
-        />
-        <Input
-          id="password"
-          type="password"
-          placeholder="비밀번호"
-          {...register("password")}
-        />
-        <Button type="submit">로그인</Button>
+    <div className="w-full h-full bg-auth bg-cover bg-center flex items-center justify-center">
+      <form
+        onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
+        className="bg-white w-[27rem] h-[35rem] px-6 py-10 space-y-reverse flex flex-wrap content-between border border-gray-300 rounded-[8px]"
+      >
+        <div>
+          <h1 className="text-4xl text-green-500 font-['SemiBold'] leading-[150%]">
+            미소
+          </h1>
+          <span className="text-2xl text-gray-300 font-['Regular']">
+            환경을 웃음으로 바꾸다 :)
+          </span>
+        </div>
+        <div className="w-full space-y-4">
+          <Input
+            id="email"
+            type="text"
+            placeholder="이메일"
+            {...register("email")}
+          />
+          <Input
+            className="placeholder-gray-500"
+            id="password"
+            type="password"
+            placeholder="비밀번호"
+            {...register("password")}
+          />
+        </div>
+        <Button
+          type="submit"
+          className="w-full bg-green-500 text-white h-[3.25rem] text-lg rounded-[8px]"
+          variant="ghost"
+          font="lg"
+          size="full"
+        >
+          로그인
+        </Button>
       </form>
-    </main>
+    </div>
   );
 }
