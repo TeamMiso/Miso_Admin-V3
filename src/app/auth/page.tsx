ce4f3495705ceb23d71e3c0b1b1c1f13c078959c
송현우 <shw9052@naver.com>
2024-08-28 09:26:07 +0900

    Add signIn input errors messages

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,22 +1,29 @@
 "use client";
 
 import { Input, Button } from "@/components/ui";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface FormValues {
+  email: string;
+  password: string;
+}
 
 export default function Auth() {
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
-  } = useForm();
+    formState: { isSubmitting, errors },
+  } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    await new Promise((r) => setTimeout(r, 1_000));
+    alert(JSON.stringify(data));
+  };
 
   return (
     <div className="w-full h-full bg-auth bg-cover bg-center flex items-center justify-center">
       <form
-        onSubmit={handleSubmit(async (data) => {
-          await new Promise((r) => setTimeout(r, 1_000));
-          alert(JSON.stringify(data));
-        })}
+        onSubmit={handleSubmit(onSubmit)}
         className="bg-white w-[27rem] h-[35rem] px-6 py-10 space-y-reverse flex flex-wrap content-between border border-gray-300 rounded-[8px]"
       >
         <div>
@@ -27,20 +34,51 @@ export default function Auth() {
             환경을 웃음으로 바꾸다 :)
           </span>
         </div>
-        <div className="w-full space-y-4">
+        <div className="w-full">
           <Input
             id="email"
             type="text"
             placeholder="이메일"
-            {...register("email")}
+            {...register("email", {
+              required: "이메일을 입력해주세요.",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "이메일 형식에 맞게 입력해주세요.",
+              },
+            })}
           />
+          {errors.email && (
+            <small className="absolute" role="alert">
+              {errors.email.message}
+            </small>
+          )}
           <Input
-            className="placeholder-gray-500"
+            className="placeholder-gray-500 mt-6"
             id="password"
             type="password"
             placeholder="비밀번호"
-            {...register("password")}
+            {...register("password", {
+              required: "비밀번호를 입력해주세요.",
+              minLength: {
+                value: 8,
+                message: "8자리 이상의 비밀번호를 작성해주세요.",
+              },
+              maxLength: {
+                value: 24,
+                message: "24자리 이하의 비밀번호를 작성해주세요.",
+              },
+              pattern: {
+                value:
+                  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-])/,
+                message: "비밀번호 형식에 맞게 입력해주세요.",
+              },
+            })}
           />
+          {errors.password && (
+            <small className="absolute" role="alert">
+              {errors.password.message}
+            </small>
+          )}
         </div>
         <Button
           type="submit"
